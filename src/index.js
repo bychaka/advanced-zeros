81e4dd97fabc44b7971108273a1c67bd366c1fb2
bychaka <aliaksandr.bychak@gmail.com>
2019-03-24 23:48:08 +0300

    feat: add getZeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let result = [];
+  for (let i = 2; i <= base; i++){
+    if (base % i === 0){
+      let divider_1 = 0;
+      while (base % i === 0){
+        divider_1++;
+        base /= i;
+      }
+      let divider_2 = 0;
+      let count_divider = number;
+      while (count_divider / i > 0){
+        divider_2 += count_divider / i;
+        count_divider /= i;
+      }
+      result.push(divider_2/divider_1);
+    }
+  }
+  return (Math.max.apply(null, result));
 }
